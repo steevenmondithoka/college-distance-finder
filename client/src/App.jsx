e2e23e94dabc2319569054a7b8293a6da828146b
steevenmondithoka <steevemondithoka@gmail.com>
2025-09-18 16:45:56 +0530

    Initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import CollegeSelect from './pages/CollegeSelect';
 
 export default function App() {
-  const [count, setCount] = useState(0);
+ 
 
   return (
   
